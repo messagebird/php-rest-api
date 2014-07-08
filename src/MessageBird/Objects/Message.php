638cd86204029c76966befab3a26f8bab4c9417a
Bob Violier <bob@violier.eu>
2014-07-08 20:34:59 +0200

    Add a recipient object that is used when a message is loaded fraom array

--- src/MessageBird/Objects/Message.php
+++ src/MessageBird/Objects/Message.php
@@ -16,29 +16,113 @@ class Message extends Base
     const DATACODING_UNICODE = 'unicode';
     const DATACODING_PLAIN = 'plain';
 
+    /**
+     * An unique random ID which is created on the MessageBird
+     * platform and is returned upon creation of the object.
+     *
+     * @var string
+     */
     protected $id;
 
+    /**
+     * Tells you if the message is sent or received.
+     * mt: mobile terminated (sent to mobile)
+     * mo: mobile originated (received from mobile)
+     *
+     * @var string
+     */
     public $direction = 'mt';
+
+    /**
+     * The type of message. Values can be: sms, binary, premium, or flash
+     *
+     * @var string
+     */
     public $type = self::TYPE_SMS;
 
+    /**
+     * The sender of the message. This can be a telephone number
+     * (including country code) or an alphanumeric string. In case
+     * of an alphanumeric string, the maximum length is 11 characters.
+     *
+     * @var string
+     */
     public $originator;
+
+    /**
+     * The body of the SMS message.
+     *
+     * @var string
+     */
     public $body;
+
+    /**
+     * A client reference. Here you can put your own reference,
+     * like your internal reference.
+     *
+     * @var string
+     */
     public $reference;
+
+    /**
+     * The amount of seconds that the message is valid.
+     * If a message is not delivered within this time,
+     * the message will be discarded.
+     *
+     * @var int
+     */
     public $validity;
 
+    /**
+     * The SMS route that is used to send the message. This is for
+     * advanced users.
+     *
+     * @var int
+     */
     public $gateway;
 
-    public $mclass = 1;
-    public $datacoding = self::DATACODING_PLAIN;
-
+    /**
+     * A hash with extra information. Is only used when a binary or premium
+     * message is sent.
+     *
+     * @var array
+     */
     public $typeDetails = array ();
 
-    public $reportUrl;
+    /**
+     * The datacoding used, can be plain or unicode
+     *
+     * @var string
+     */
+    public $datacoding = self::DATACODING_PLAIN;
 
+    /**
+     * Indicates the message type. 1 is a normal message, 0 is a flash message.
+     *
+     * @var int
+     */
+    public $mclass = 1;
+
+    /**
+     * The scheduled date and time of the message in RFC3339 format (Y-m-d\TH:i:sP)
+     *
+     * @var string
+     */
     public $scheduledDatetime;
+
+    /**
+     * The date and time of the creation of the message in RFC3339 format (Y-m-d\TH:i:sP)
+     * @var string
+     */
     protected $createdDatetime;
 
+    /**
+     * An array of recipients
+     *
+     * @var array
+     */
     public $recipients = array ();
+
 
     /**
      * Send a premium SMS
@@ -89,5 +173,26 @@ class Message extends Base
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * @param $object
+     *
+     * @return $this|void
+     */
+    public function loadFromArray ($object)
+    {
+        parent::loadFromArray($object);
+
+        if (!empty($this->recipients->items)) {
+            foreach($this->recipients->items AS &$item) {
+                $Recipient = new Recipient();
+                $Recipient->loadFromArray($item);
+
+                $item = $Recipient;
+            }
+        }
+
+        return $this;
     }
 }
---
+++ src/MessageBird/Objects/Recipient.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace MessageBird\Objects;
+
+/**
+ * Class Recipient
+ *
+ * @package MessageBird\Objects
+ */
+class Recipient extends Base
+{
+    /**
+     * The msisdn of the recipient
+     *
+     * @var int
+     */
+    public $recipient;
+
+    /**
+     * The status of the message sent to the recipient.
+     * Possible values for SMS are: scheduled, sent, buffered,
+     * delivered, delivery_failed
+     *
+     * Possible values for TTS are: calling, answered,
+     * failed, busy, machine
+     *
+     * @var string
+     */
+    public $status;
+
+    /**
+     * The datum time of the last status in RFC3339 format (Y-m-d\TH:i:sP)
+     *
+     * @var string
+     */
+    public $statusDatetime;
+}
