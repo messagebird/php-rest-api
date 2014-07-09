88e011a02882fb931b7f43ffdff3ed2c3d8ab36b
Bob Violier <bob@violier.eu>
2014-07-09 11:47:38 +0200

    Add inline comments to Balance and Hlr

--- src/MessageBird/Objects/Balance.php
+++ src/MessageBird/Objects/Balance.php
@@ -9,7 +9,24 @@ namespace MessageBird\Objects;
  */
 class Balance extends Base
 {
+    /**
+     * Your payment method. Possible values are: prepaid & postpaid
+     *
+     * @var string
+     */
     public $payment;
+
+    /**
+     * Your payment type. Possible values are: credits & euros
+     *
+     * @var string
+     */
     public $type;
+
+    /**
+     * The amount of balance of the payment type. When postpaid is your payment method, the amount will be 0.
+     *
+     * @var float
+     */
     public $amount;
 }
--- src/MessageBird/Objects/Hlr.php
+++ src/MessageBird/Objects/Hlr.php
@@ -15,14 +15,60 @@ class Hlr extends Base
     const STATUS_UNKNOWN = 'unknown';
     const STATUS_FAILED = 'failed';
 
+    /**
+     * An unique random ID which is created on the MessageBird platform and is returned upon creation of the object.
+     *
+     * @var string
+     */
     protected $id;
+
+    /**
+     * The URL of the created object.
+     *
+     * @var string
+     */
     protected $href;
 
+    /**
+     * The msisdns you want to do a network query on
+     *
+     * @var int
+     */
     public $msisdn;
+
+    /**
+     * The MCCMNC code of the network provider. (http://en.wikipedia.org/wiki/Mobile_country_code)
+     *
+     * @var int
+     */
     public $network;
+
+    /**
+     * A client reference. Here you can put your own reference,
+     * like your internal reference.
+     *
+     * @var string
+     */
     public $reference;
+
+    /**
+     * The status of the msisdns. Possible values: sent, absent, active, unknown, and failed
+     *
+     * @var string
+     */
     public $status;
 
+    /**
+     * The date and time of the creation of the hlr in RFC3339 format (Y-m-d\TH:i:sP)
+     *
+     * @var string
+     */
     protected $createdDatetime;
+
+    /**
+     * The datum time of the last status in RFC3339 format (Y-m-d\TH:i:sP)
+     *
+     * @var string
+     */
     protected $statusDatetime;
 }
