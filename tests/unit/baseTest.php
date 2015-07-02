59c8a92042c9d731745df0822074ae0858cf3869
Dennis van der Vliet <dennis.vandervliet@gmail.com>
2015-07-02 17:39:31 +0200

    Update base test

--- tests/unit/baseTest.php
+++ tests/unit/baseTest.php
@@ -4,6 +4,10 @@ class BaseTest extends PHPUnit_Framework_TestCase
     public function testClientConstructor()
     {
         $MessageBird = new \MessageBird\Client('YOUR_ACCESS_KEY');
-        
+        $this->assertInstanceOf('MessageBird\Resources\Balance', $MessageBird->balance);
+        $this->assertInstanceOf('MessageBird\Resources\Hlr', $MessageBird->hlr);
+        $this->assertInstanceOf('MessageBird\Resources\Messages', $MessageBird->messages);
+        $this->assertInstanceOf('MessageBird\Resources\VoiceMessage', $MessageBird->voicemessages);
+        $this->assertInstanceOf('MessageBird\Resources\Otp', $MessageBird->otp);
     }
 }
