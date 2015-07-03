e429b40d9eade2cbb04fba1e9cf70b87b744ff92
Dennis van der Vliet <dennis.vandervliet@gmail.com>
2015-07-03 13:29:19 +0200

    Add tests for Balance, Hlr and Voice

---
+++ tests/unit/balance/BalanceTest.php
@@ -0,0 +1,17 @@
+<?php
+class BalanceTest extends BaseTest
+{
+
+    public function setUp(){
+        parent::setup();
+        $this->client = new \MessageBird\Client('YOUR_ACCESS_KEY', $this->mockClient);
+    }
+    /**
+     * @expectedException     MessageBird\Exceptions\ServerException
+    */
+    public function testReadBalance()
+    {
+        $this->mockClient->expects($this->once())->method('performHttpRequest')->with("GET", 'balance', null, null);
+        $Hlr = $this->client->balance->read();
+    }
+}
