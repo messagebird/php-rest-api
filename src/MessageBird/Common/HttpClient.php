b79b426f4d28a2fa228dc109e2c4ac5963da2b2e
Daniil Belyakov <daniil@bunq.com>
2017-03-30 13:23:38 +0200

    Refactor HttpClient

--- src/MessageBird/Common/HttpClient.php
+++ src/MessageBird/Common/HttpClient.php
@@ -6,18 +6,33 @@ use MessageBird\Exceptions;
 use MessageBird\Common;
 
 /**
- * Class HttpClient
- *
  * @package MessageBird\Common
  */
 class HttpClient
 {
+    /**
+     * Request method constants.
+     */
     const REQUEST_GET = 'GET';
     const REQUEST_POST = 'POST';
     const REQUEST_DELETE = 'DELETE';
     const REQUEST_PUT = 'PUT';
 
+    /**
+     * Response code constants.
+     */
     const HTTP_NO_CONTENT = 204;
+
+    /**
+     * Relative path to CA Bundle file.
+     */
+    const CA_BUNDLE_PATH_RELATIVE = '/../ca-bundle.crt';
+
+    /**
+     * Default timeout values.
+     */
+    const TIMEOUT_DEFAULT = 10;
+    const CONNECTION_TIMEOUT_DEFAULT = 2;
 
     /**
      * @var string
@@ -37,12 +52,17 @@ class HttpClient
     /**
      * @var int
      */
-    private $timeout = 10;
+    private $timeout = self::TIMEOUT_DEFAULT;
 
     /**
      * @var int
      */
-    private $connectionTimeout = 2;
+    private $connectionTimeout = self::CONNECTION_TIMEOUT_DEFAULT;
+
+    /**
+     * @var resource
+     */
+    private $curl;
 
     /**
      * @param string $endpoint
@@ -51,27 +71,85 @@ class HttpClient
      *
      * @throws \InvalidArgumentException if timeout settings are invalid
      */
-    public function __construct($endpoint, $timeout = 10, $connectionTimeout = 2)
-    {
+    public function __construct(
+        $endpoint,
+        $timeout = self::TIMEOUT_DEFAULT,
+        $connectionTimeout = self::CONNECTION_TIMEOUT_DEFAULT
+    ) {
+        $this->assertTimeoutValid($timeout);
+        $this->assertConnectionTimeoutValid($connectionTimeout);
+
         $this->endpoint = $endpoint;
-
-        if (!is_int($timeout) || $timeout < 1) {
-            throw new \InvalidArgumentException(sprintf(
-                'Timeout must be an int > 0, got "%s".',
-                is_object($timeout) ? get_class($timeout) : gettype($timeout).' '.var_export($timeout, true))
-            );
-        }
-
         $this->timeout = $timeout;
+        $this->connectionTimeout = $connectionTimeout;
+    }
 
-        if (!is_int($connectionTimeout) || $connectionTimeout < 0) {
-            throw new \InvalidArgumentException(sprintf(
+    /**
+     * @param $timeout
+     */
+    private function assertTimeoutValid($timeout)
+    {
+        if (!$this->isValidTimeout($timeout)) {
+            throw $this->createInvalidArgumentTypeException('Timeout must be an int > 0, got "%s".', $timeout);
+        }
+    }
+
+    /**
+     * @param $timeout
+     *
+     * @return bool
+     */
+    private function isValidTimeout($timeout)
+    {
+        return is_int($timeout) && $timeout > 0;
+    }
+
+    /**
+     * @param string $message
+     * @param mixed $argument
+     *
+     * @return \InvalidArgumentException
+     */
+    private function createInvalidArgumentTypeException($message, $argument)
+    {
+        return new \InvalidArgumentException(sprintf($message, $this->getTypeString($argument)));
+    }
+
+    /**
+     * @param $argument
+     *
+     * @return string
+     */
+    private function getTypeString($argument)
+    {
+        if (is_object($argument)) {
+            return get_class($argument);
+        } else {
+            return gettype($argument) . ' ' . var_export($argument, true);
+        }
+    }
+
+    /**
+     * @param $connectionTimeout
+     */
+    private function assertConnectionTimeoutValid($connectionTimeout)
+    {
+        if (!$this->isValidConnectionTimeout($connectionTimeout)) {
+            throw $this->createInvalidArgumentTypeException(
                 'Connection timeout must be an int >= 0, got "%s".',
-                is_object($connectionTimeout) ? get_class($connectionTimeout) : gettype($connectionTimeout).' '.var_export($connectionTimeout, true))
+                $connectionTimeout
             );
         }
+    }
 
-        $this->connectionTimeout = $connectionTimeout;
+    /**
+     * @param $connectionTimeout
+     *
+     * @return bool
+     */
+    private function isValidConnectionTimeout($connectionTimeout)
+    {
+        return is_int($connectionTimeout) && $connectionTimeout >= 0;
     }
 
     /**
@@ -91,28 +169,9 @@ class HttpClient
     }
 
     /**
+     * @param string $method
      * @param string $resourceName
-     * @param mixed  $query
-     *
-     * @return string
-     */
-    public function getRequestUrl($resourceName, $query)
-    {
-        $requestUrl = $this->endpoint . '/' . $resourceName;
-        if ($query) {
-            if (is_array($query)) {
-                $query = http_build_query($query);
-            }
-            $requestUrl .= '?' . $query;
-        }
-
-        return $requestUrl;
-    }
-
-    /**
-     * @param string      $method
-     * @param string      $resourceName
-     * @param mixed       $query
+     * @param mixed $query
      * @param string|null $body
      *
      * @return array
@@ -122,61 +181,225 @@ class HttpClient
      */
     public function performHttpRequest($method, $resourceName, $query = null, $body = null)
     {
-        $curl = curl_init();
+        $this->assertHasAuthentication();
+        $this->openCurlConnection();
+        $this->prepareHttpRequest($method, $resourceName, $query, $body);
+        $responseArray = $this->executeHttpRequest();
+        $this->closeCurlConnection();
 
+        return $responseArray;
+    }
+
+    /**
+     * @throws Exceptions\AuthenticateException
+     */
+    private function assertHasAuthentication()
+    {
         if ($this->Authentication === null) {
             throw new Exceptions\AuthenticateException('Can not perform API Request without Authentication');
         }
+    }
 
-        $headers = array (
+    /**
+     */
+    private function openCurlConnection()
+    {
+        $this->curl = curl_init();
+    }
+
+    /**
+     * @param string $method
+     * @param string $resourceName
+     * @param mixed $query
+     * @param string|null $body
+     */
+    private function prepareHttpRequest($method, $resourceName, $query = null, $body = null)
+    {
+        $this->setCommonCurlOptions($resourceName, $query);
+        $this->setMethodSpecificCurlOptions($method, $body);
+        // Some servers have outdated or incorrect certificates, Use the included CA-bundle
+        $this->setCurlCaBundle();
+    }
+
+    /**
+     * @param $resourceName
+     * @param mixed $query
+     */
+    private function setCommonCurlOptions($resourceName, $query = null)
+    {
+        curl_setopt($this->curl, CURLOPT_HTTPHEADER, $this->getHeaderArray());
+        curl_setopt($this->curl, CURLOPT_HEADER, true);
+        curl_setopt($this->curl, CURLOPT_URL, $this->getRequestUrl($resourceName, $query));
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->curl, CURLOPT_TIMEOUT, $this->timeout);
+        curl_setopt($this->curl, CURLOPT_CONNECTTIMEOUT, $this->connectionTimeout);
+    }
+
+    /**
+     * @return string[]
+     */
+    private function getHeaderArray()
+    {
+        return array(
             'User-agent: ' . implode(' ', $this->userAgent),
             'Accept: application/json',
             'Content-Type: application/json',
             'Accept-Charset: utf-8',
             sprintf('Authorization: AccessKey %s', $this->Authentication->accessKey)
         );
+    }
 
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($curl, CURLOPT_HEADER, true);
-        curl_setopt($curl, CURLOPT_URL, $this->getRequestUrl($resourceName, $query));
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_TIMEOUT, $this->timeout);
-        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $this->connectionTimeout);
+    /**
+     * @param string $resourceName
+     * @param mixed $query
+     *
+     * @return string
+     */
+    public function getRequestUrl($resourceName, $query)
+    {
+        $requestUrl = $this->endpoint . '/' . $resourceName;
 
+        if ($query) {
+            if (is_array($query)) {
+                $query = http_build_query($query);
+            }
+
+            $requestUrl .= '?' . $query;
+        }
+
+        return $requestUrl;
+    }
+
+    /**
+     * @param $method
+     * @param string|null $body
+     */
+    private function setMethodSpecificCurlOptions($method, $body = null)
+    {
         if ($method === self::REQUEST_GET) {
-            curl_setopt($curl, CURLOPT_HTTPGET, true);
+            curl_setopt($this->curl, CURLOPT_HTTPGET, true);
         } elseif ($method === self::REQUEST_POST) {
-            curl_setopt($curl, CURLOPT_POST, true);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $body);
+            curl_setopt($this->curl, CURLOPT_POST, true);
+            curl_setopt($this->curl, CURLOPT_POSTFIELDS, $body);
         } elseif ($method === self::REQUEST_DELETE) {
-            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, self::REQUEST_DELETE);
-        } elseif ($method === self::REQUEST_PUT){
-            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, self::REQUEST_PUT);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $body);
+            curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, self::REQUEST_DELETE);
+        } elseif ($method === self::REQUEST_PUT) {
+            curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, self::REQUEST_PUT);
+            curl_setopt($this->curl, CURLOPT_POSTFIELDS, $body);
         }
+    }
 
-        // Some servers have outdated or incorrect certificates, Use the included CA-bundle
-        $caFile = realpath(__DIR__ . '/../ca-bundle.crt');
-        if (!file_exists($caFile)) {
-            throw new Exceptions\HttpException(sprintf('Unable to find CA-bundle file "%s".', __DIR__ . '/../ca-bundle.crt'));
+    /**
+     */
+    private function setCurlCaBundle()
+    {
+        $this->assertCaBundleExists();
+
+        curl_setopt($this->curl, CURLOPT_CAINFO, $this->getCaBundleRealPath());
+    }
+
+    /**
+     * @throws Exceptions\HttpException
+     */
+    private function assertCaBundleExists()
+    {
+        if (!$this->isCaBundleFileExists()) {
+            throw new Exceptions\HttpException(
+                sprintf('Unable to find CA-bundle file "%s".', $this->getCaBundlePath())
+            );
         }
+    }
 
-        curl_setopt($curl, CURLOPT_CAINFO, $caFile);
+    /**
+     * @return bool
+     */
+    private function isCaBundleFileExists()
+    {
+        return file_exists($this->getCaBundleRealPath());
+    }
 
-        $response = curl_exec($curl);
+    /**
+     * @return string
+     */
+    private function getCaBundleRealPath()
+    {
+        return realpath($this->getCaBundlePath());
+    }
 
+    /**
+     * @return string
+     */
+    private function getCaBundlePath()
+    {
+        return __DIR__ . self::CA_BUNDLE_PATH_RELATIVE;
+    }
+
+    /**
+     * @return array
+     * @throws Exceptions\HttpException
+     */
+    private function executeHttpRequest()
+    {
+        $response = curl_exec($this->curl);
+
+        $this->assertIsValidResponse($response);
+
+        return $this->getResponseArray($response);
+    }
+
+    /**
+     * @param $response
+     *
+     * @throws Exceptions\HttpException
+     */
+    private function assertIsValidResponse($response)
+    {
         if ($response === false) {
-            throw new Exceptions\HttpException(curl_error($curl), curl_errno($curl));
+            throw new Exceptions\HttpException(curl_error($this->curl), curl_errno($this->curl));
         }
+    }
 
-        $responseStatus = (int) curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    /**
+     * @param string $response
+     *
+     * @return array
+     */
+    private function getResponseArray($response)
+    {
+        $responseStatus = $this->getCurlResponseStatus();
+        $responseHeaderAndBody = $this->splitResponseIntoHeaderAndBody($response);
 
-        // Split the header and body
+        return array_merge(array($responseStatus), $responseHeaderAndBody);
+    }
+
+    /**
+     * @return int
+     */
+    private function getCurlResponseStatus()
+    {
+        return (int)curl_getinfo($this->curl, CURLINFO_HTTP_CODE);
+    }
+
+    /**
+     * @param string $response
+     *
+     * @return array
+     */
+    private function splitResponseIntoHeaderAndBody($response)
+    {
         $parts = explode("\r\n\r\n", $response, 3);
-        list($responseHeader, $responseBody) = ($parts[0] === 'HTTP/1.1 100 Continue') ? array ($parts[1], $parts[2]) : array ($parts[0], $parts[1]);
 
-        curl_close($curl);
+        if ($parts[0] === 'HTTP/1.1 100 Continue') {
+            return array($parts[1], $parts[2]);
+        } else {
+            return array($parts[0], $parts[1]);
+        }
+    }
 
-        return array ($responseStatus, $responseHeader, $responseBody);
+    /**
+     */
+    private function closeCurlConnection()
+    {
+        curl_close($this->curl);
     }
 }
