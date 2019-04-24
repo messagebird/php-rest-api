8a9ce6dcb2be65bd1bb4b8189a890c6f0bc5a54e
Chris Minett <chris@minett.me.uk>
2019-04-24 15:32:45 +0100

    Update SignedRequest example to handle ValidationExceptions
    If expected values are missing or invalid when calling createFromGlobals() an exception is thrown.
    The example now demonstrates how an implementation can handle this and respond cleanly.

--- examples/signedrequest-verification.php
+++ examples/signedrequest-verification.php
@@ -6,7 +6,12 @@ require_once(__DIR__.'/../autoload.php');
 $requestValidator = new \MessageBird\RequestValidator('YOUR_SIGNING_KEY');
 
 // Set up the signed request from the PHP global variables.
-$request = \MessageBird\Objects\SignedRequest::createFromGlobals();
+try {
+    $request = \MessageBird\Objects\SignedRequest::createFromGlobals();
+} catch (\MessageBird\Exceptions\ValidationException $e) {
+    // The request was missing expected values, so respond accordingly.
+    http_response_code(412);
+}
 
 if (!$requestValidator->verify($request)) {
     // The request was invalid, so respond accordingly.
