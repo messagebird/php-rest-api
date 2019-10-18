ff9f5f694adb40e95fc6c61b5f0771e7b9a80987
Chris Johnston <chris.johnston@sport80.com>
2019-10-18 15:30:17 +0100

    Created Recipients object rather than it being a stdClass

--- src/MessageBird/Objects/Recipient.php
+++ src/MessageBird/Objects/Recipient.php
@@ -35,4 +35,28 @@ class Recipient extends Base
      * @var string
      */
     public $statusDatetime;
+
+    /**
+     * @return int
+     */
+    public function getRecipient()
+    {
+        return $this->recipient;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatusDatetime()
+    {
+        return $this->statusDatetime;
+    }
 }
