892c79b215c8ff31adbf03751b493b9d518b593b
Alexander Timmermann <timmermann@messagebird.com>
2020-07-16 12:26:27 +0200

    chore: bump version to 1.19.1

--- src/MessageBird/Client.php
+++ src/MessageBird/Client.php
@@ -19,7 +19,7 @@ class Client
     const ENABLE_CONVERSATIONSAPI_WHATSAPP_SANDBOX = 'ENABLE_CONVERSATIONSAPI_WHATSAPP_SANDBOX';
     const CONVERSATIONSAPI_WHATSAPP_SANDBOX_ENDPOINT = 'https://whatsapp-sandbox.messagebird.com/v1';
 
-    const CLIENT_VERSION = '1.18.0';
+    const CLIENT_VERSION = '1.19.1';
 
     /**
      * @var string
