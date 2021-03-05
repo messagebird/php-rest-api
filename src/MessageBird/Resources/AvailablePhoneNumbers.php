877366a3e78922077cf3aa97131fb109fb90d724
Lucas Michot <lucas.michot@shopify.com>
2021-03-05 16:38:30 +0100

    Fix loadFromArray call

--- src/MessageBird/Resources/AvailablePhoneNumbers.php
+++ src/MessageBird/Resources/AvailablePhoneNumbers.php
@@ -54,7 +54,7 @@ class AvailablePhoneNumbers
         $baseList->loadFromArray($body);
 
         foreach ($items as $item) {
-            $object = new Objects\Number($this->HttpClient);
+            $object = new Objects\Number();
 
             $itemObject = $object->loadFromArray($item);
             $baseList->items[] = $itemObject;
@@ -82,7 +82,7 @@ class AvailablePhoneNumbers
             throw new \MessageBird\Exceptions\RequestException($ResponseError->getErrorString());
         }
 
-        return Objects\Number.loadFromArray($body->data[0]);
+        return (new Objects\Number())->loadFromArray($body->data[0]);
     }
 }
 ?>
