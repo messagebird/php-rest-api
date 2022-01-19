2601a5c5c17ce9908d4900145688343acb16d08c
Dennis van der Vliet <dennis@wetransfer.com>
2022-01-19 07:44:18 +0100

    Add additional properties to Recipient object (#186)
    * Add additional properties to Recipient object
    
    The REST API returns attributes not included in the `Recipient` object. This PR
    adds additional attributes with one exception; pricing information.
    
    Co-authored-by: Dennis van der Vliet <dennis.vandervliet@messagebird.com>
    Co-authored-by: Alexandru Bucur <alex.bucur@messagebird.com>

--- src/MessageBird/Objects/Recipient.php
+++ src/MessageBird/Objects/Recipient.php
@@ -29,9 +29,85 @@ class Recipient extends Base
     public $status;
 
     /**
-     * The datum time of the last status in RFC3339 format (Y-m-d\TH:i:sP)
+     * The date and time of the last status in RFC3339 format (Y-m-d\TH:i:sP)
      *
      * @var string
      */
     public $statusDatetime;
+
+
+    /**
+     * The details about the status message. More details can be found here:
+     * https://developers.messagebird.com/api/sms-messaging/#sms-statuses
+     *
+     * @var string
+     */
+    public $statusReason;
+
+    /**
+     * Extra error code that describes the failure in more detail (optional, 
+     * null if not available)
+     *
+     * @var string
+     */
+    public $statusErrorCode;
+
+    /**
+     * The name of the recipient’s original country, based on MSISDN.
+     *
+     * @var string
+     */
+    public $recipientCountry;
+
+    /**
+     * The prefix code for the recipient’s original country, based on MSISDN.
+     *
+     * @var int
+     */
+    public $recipientCountryPrefix;
+
+    /**
+     * The name of the operator of the recipient. Identified by MCCMNC 
+     * of the message.
+     *
+     * @var string
+     */
+    public $recipientOperator;
+
+    /**
+     * The code of the operator of the message sender. 
+     * It could have null value if the message isn’t delivered yet.
+     *
+     * @var string
+     */
+    public $mccmnc;
+
+    /**
+     * The MCC (Mobile Country Code) part of MCCMNC.
+     *
+     * @var string
+     */
+    public $mcc;
+
+    /**
+     * The MNC (Mobile Network Code) part of MCCMNC.
+     *
+     * @var string
+     */
+    public $mnc;
+    
+    /**
+     * The length of the message in characters. Depends on the 
+     * message datacoding.
+     *
+     * @var int
+     */
+    public $messageLength;
+
+    /**
+     * The count of total messages send. Personalisation not taken in account.
+     *
+     * @var int
+     */
+    public $messagePartCount;
 }
