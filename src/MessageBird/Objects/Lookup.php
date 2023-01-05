951fda28a67723a4b70ae9104c4780db84e2f9bb
Alexandru Bucur <alex.bucur@messagebird.com>
2023-01-05 20:54:10 +0100

    bugfix #207 format/hlr to stdClass type

--- src/MessageBird/Objects/Lookup.php
+++ src/MessageBird/Objects/Lookup.php
@@ -58,9 +58,9 @@ class Lookup extends Base
     protected $type;
 
     /**
-     * An associative array containing references to this phone number in several different formats.
+     * An object containing references to this phone number in several formats.
      *
-     * @var array
+     * @var stdClass
      *
      * e164: The phone number in E.164 format.
      * international: The phone number in international format.
@@ -72,7 +72,7 @@ class Lookup extends Base
     /**
      * The most recent HLR object. If no such HLR objects exists, this array won't be returned.
      *
-     * @var array
+     * @var stdClass
      *
      * id(string): An unique random ID which is created on the MessageBird platform.
      * network(int): The MCCMNC code of the network provider.
@@ -108,19 +108,19 @@ class Lookup extends Base
         return $this->type;
     }
 
-    public function getFormats(): ?array
+    public function getFormats(): ?stdClass
     {
         return $this->formats;
     }
 
-    public function getHLR(): ?array
+    public function getHLR(): ?stdClass
     {
         return $this->hlr;
     }
 
     /**
      * @deprecated 2.2.0 No longer used by internal code, please switch to {@see self::loadFromStdclass()}
-     * 
+     *
      * @param mixed $object
      *
      * @return $this
